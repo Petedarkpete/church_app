aa00385d430ddb99173b2500e9122a8e21f2e0c3
Petedarkpete <peterndahi2018@gmail.com>
2025-06-22 20:43:04 +0300

    pastorials

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header"
 import HeroSection from "@/components/hero-section"
 import ServiceTimes from "@/components/service-times"
+import Testimonials from "@/components/testimonials"
 import AboutSection from "@/components/about-section"
 import Ministries from "@/components/ministries"
 import Events from "@/components/events"
 import ContactSection from "@/components/contact-section"
 import Footer from "@/components/footer"
+
 import "animate.css"
 import FadeInSection from "@/components/FadeInSection";
 
@@ -17,6 +19,8 @@ export default function ChurchWebsite() {
       <HeroSection />
       <FadeInSection><ServiceTimes /></FadeInSection>
       <FadeInSection><AboutSection /></FadeInSection>
+      <FadeInSection><Testimonials /></FadeInSection>
+      
       <FadeInSection><Ministries /></FadeInSection>
       <FadeInSection><Events /></FadeInSection>
       <FadeInSection><ContactSection /></FadeInSection>
